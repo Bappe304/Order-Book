40ccffa74fcbaafd7ad4b0e933e4b4e38f03ea08
Vedansh Bansal <bansalskb2@gmail.com>
2024-09-27 16:17:18 +0800

    int main

--- Orderbook.cpp
+++ Orderbook.cpp
@@ -394,5 +394,11 @@ private:
 
 int main()
 {
+    Orderbook orderbook;
+    const OrderID orderId = 1;
+    orderbook.AddOrder(std::make_shared<Order>(OrderType::GoodTillCancel, orderId, Side::Buy, 100, 10));
+    std::cout << orderbook.Size() << std::endl; // 1
+    orderbook.CancelOrder(orderId);
+    std::cout << orderbook.Size() << std::endl; // 0
     return 0;
 }
